3318682812c4fcc657fc810b17492086cbd34373
ZanY3 <146362968+ZanY3@users.noreply.github.com>
2024-07-27 14:58:49 +0300

    End

---
+++ Assets/Scripts/Controllers/EndController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndController : MonoBehaviour
+{
+    public int enemyesLeft = 2;
+    public string loadScene = "EndTrailer";
+
+    private async void Update()
+    {
+        if(enemyesLeft <= 0)
+        {
+            await new WaitForSeconds(3f);
+            SceneManager.LoadScene(loadScene);
+        }
+    }
+}
--- Assets/Scripts/Controllers/Health.cs
+++ Assets/Scripts/Controllers/Health.cs
@@ -13,16 +13,23 @@ public class Health : MonoBehaviour
     public GameObject damageParticles;
 
     public GameObject finishPortal;
+    private EndController endController;
+
+    private void Start()
+    {
+        endController = FindAnyObjectByType<EndController>();
+    }
 
     public void TakeDamage(float damage)
     {
+        float randPitch = Random.Range(0.8f, 1);
+        dieSource.pitch = randPitch;
         Instantiate(damageParticles, transform.position, Quaternion.identity);
         dieSource.PlayOneShot(damageSound);
         health -= damage;
         if(health <= 0)
         {
-            float randPitch = Random.Range(0.5f, 1);
-            dieSource.pitch = randPitch;
+            endController.enemyesLeft--;
             dieSource.PlayOneShot(dieSound);
             Instantiate(dieParticles, transform.position, Quaternion.identity);
             finishPortal.SetActive(true);
